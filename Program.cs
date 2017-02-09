7f6e84204ac2a78c7cce2ce5ad92476afc093756
Dan Mann <danielmann146@hotmail.co.uk>
2017-02-09 11:48:19 +0000

    Initial commit of project.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Report Fixer! Enter the path of a report to begin tidying.");
+            var fixer = new ReportFixer();
+            var reportPath = Console.ReadLine();
+            fixer.LoadReport(reportPath);
+            Console.WriteLine("Processing report...");
+            fixer.FixUnits();
+            Console.WriteLine($"Processed report at: {reportPath}");
+
+            // if (numberOfIssuesFixed == 0)
+            // {
+            //     Console.WriteLine("No issues were found.");
+            // }
+            // else if (numberOfIssuesFixed == 1) {
+            //     Console.WriteLine("1 issue was fixed.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"{numberOfIssuesFixed} issues were fixed.");
+            // }
+        }
+    }
+}
---
+++ ReportFixer.cs
@@ -0,0 +1,87 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication
+{
+    class ReportFixer
+    {
+        private string _report;
+
+        private int _numberOfIssuesFixed;
+
+        public void LoadReport(string report)
+        {
+            this._report = report;
+            this._numberOfIssuesFixed = 0;
+        }
+
+        public void FixUnits()
+        {
+            // Create a temporary file path where we can write modify lines.
+            string temporaryFile = Path.Combine(Path.GetDirectoryName(this._report),
+                "report-temp.rdl");
+
+            int numberOfIssuesFixed = 0;
+
+            // Open a stream for the temporary file.
+            using (var temporaryFileStream = new StreamWriter(File.OpenWrite(temporaryFile)))
+            {
+                using (var sourceFile = File.OpenText(this._report))
+                {
+                    string line;
+
+                    // Read lines while the file has them.
+                    while ((line = sourceFile.ReadLine()) != null)
+                    {
+                        // Do the pattern replacement.
+                        ConvertCmToMm(ref line);
+                        RoundDecimals(ref line);
+
+
+                        var reportUnitPattern = @"<rd:ReportUnitType>Cm</rd:ReportUnitType>";
+
+                        line = Regex.Replace(line, reportUnitPattern,
+                            match =>
+                            {
+                                numberOfIssuesFixed++;
+                                return @"<rd:ReportUnitType>Mm</rd:ReportUnitType>";
+                            });
+
+                        // Write the modified line to the new file.
+                        temporaryFileStream.WriteLine(line);
+                    }
+                }
+            }
+            // Replace current with temporary.
+            File.Delete(this._report);
+            File.Move(temporaryFile, this._report);
+        }
+
+        private void ConvertCmToMm(ref string line)
+        {
+            var pattern = @"(\d+(\.\d+)?)cm";
+
+            line = Regex.Replace(line, pattern,
+                match =>
+                {   this._numberOfIssuesFixed++;
+                    var number = double.Parse(match.Groups[1].Value);
+                    return (number * 10).ToString() + "mm";
+                });
+        }
+
+        private void RoundDecimals(ref string line)
+        {
+            var pattern = @"(\d+\.\d+)mm";
+
+            line = Regex.Replace(line, pattern,
+                match =>
+                {
+                    this._numberOfIssuesFixed++;
+                    var number = double.Parse(match.Groups[1].Value);
+                    number = Math.Round(number, 0, MidpointRounding.AwayFromZero);
+                    return (number).ToString() + "mm";
+                });
+        }
+    }
+}

ef3e49c2edff15d1aff3eb4b83e8cfb50394c1c5
Dan Mann <danielmann146@hotmail.co.uk>
2017-02-24 10:50:38 +0000

    Added comments to the Report class.

--- Report.cs
+++ Report.cs
@@ -1,11 +1,27 @@
 namespace ConsoleApplication
 {
+    /// <summary>
+    /// The Report class is used keep track of a report's file location and the number of issues
+    /// detected and resolved in the XML content of the report.
+    /// </summary>
     class Report
     {
+        /// <summary>
+        /// The local file path of the report.
+        /// </summary>
+        /// <returns>Report path.</returns>
         public string Path { get; private set; }
 
+        /// <summary>
+        /// Number of style or formatting issues fixed in the XML content of the report.
+        /// </summary>
+        /// <returns>Count of issues fixed.</returns>
         public int NumberOfIssuesFixed { get; set; }
 
+        /// <summary>
+        /// Initialises a report with a local path and number of issues set to 0.
+        /// </summary>
+        /// <param name="path">The local file path of the report.</param>
         public Report(string path)
         {
             this.Path = path;
